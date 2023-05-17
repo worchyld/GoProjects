3dd3bf4c75d5e503481d184e69bb8712e00cee03
AD <silentflute.ad@gmail.com>
2023-05-17 17:15:13 +0100

    Adding addition

--- HelloWorld/hello.go
+++ HelloWorld/hello.go
@@ -3,5 +3,12 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("hello world");
+	fmt.Println("hello world")
+
+	// Simple addition
+	var x int = 5
+	var y int = 7
+	var sum int = x + y
+
+	fmt.Println(sum)
 }
